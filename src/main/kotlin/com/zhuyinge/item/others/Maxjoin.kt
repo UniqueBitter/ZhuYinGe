6b9d39687e855728b762b7c0b25af38c3f750d25
Unique <1911735906@qq.com>
2023-05-04 22:35:22 +0800

    添加了满人检测

--- src/main/kotlin/com/zhuyinge/item/others/Maxjoin.kt
+++ src/main/kotlin/com/zhuyinge/item/others/Maxjoin.kt
@@ -1,17 +1,26 @@
 package com.zhuyinge.item.others
 
-import org.bukkit.event.EventHandler
-import org.bukkit.event.EventPriority
 import org.bukkit.event.player.PlayerLoginEvent
+import taboolib.common.platform.event.EventPriority
 import taboolib.common.platform.event.SubscribeEvent
 
 
 object Maxjoin {
-    @SubscribeEvent
-    @EventHandler(priority = EventPriority.HIGHEST)
+    private val whitelist = mutableListOf(
+        "Lanterns_beiren",
+        "Unique_Bitter",
+        "xiaoxiaoOWO",
+        "lonely0719"
+    )
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     fun onLogin(event: PlayerLoginEvent) {
         if (event.result == PlayerLoginEvent.Result.KICK_FULL) {
-            event.allow()
+            val player = event.player
+            val isOp = player.isOp
+            if (isOp || whitelist.contains(player.name)) {
+                event.allow()
+            }
         }
     }
 }
